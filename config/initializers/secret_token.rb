2fac3a36f5a16f36893f62deae48c041f160f692
andrewjbryk <ajbryk@gmail.com>
2012-08-27 19:20:31 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splashdown::Application.config.secret_token = 'b6d72c7689c97d26318620bdcca4a85c96cbfe8511f3bbcb20a30fa410954f50fa991b69552006f7cd1dbcb36c57e7ac68b5d8c3d16ff728eb938c296f71f9af'
